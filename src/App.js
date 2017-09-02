4ddd21517268141bc8a8e6b9dc157715f865b81e
Chris Budiman <cbudiman.ca@gmail.com>
2017-09-02 12:50:02 -0400

    refactor: Organize files by type

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import * as BooksAPI from './BooksAPI'
-import ListBooks from './ListBooks'
-import SearchBooks from './SearchBook'
+import * as BooksAPI from './utils/BooksAPI'
+import ListBooks from './components/ListBooks'
+import SearchBooks from './components/SearchBook'
 import { Route } from 'react-router-dom'
 import './App.css'
 
